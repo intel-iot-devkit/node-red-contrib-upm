4ad2c10173cf25db94e5ceaef082764ef9e8f021
Scott Ware <scott2.ware@gmail.com>
2016-04-22 11:56:55 +0100

    Assume imraa is available and will handle hardware detection and initialisation.

--- curieimu.js
+++ curieimu.js
@@ -1,41 +1,13 @@
 module.exports = function(RED){
 
     var mraa = require('mraa');
-    var firmata = require('arduino-firmata');
     var curieImu = require('jsupm_curieimu');
-
-    function ArduinoDevice(n) {
-        RED.nodes.createNode(this,n);
-        this.device = n.device || null;
-
-        var node = this;
-
-        firmata.list(function (err, devices) {
-            if (devices.length == 0) {
-                node.warn(RED._("No devices found!"));
-            } else if (!node.device) {
-		node.log(RED._("No device specified. Connecting to first device found __device__", {device:devices[0]}));
-		mraa.addSubplatform(mraa.GENERIC_FIRMATA, devices[0]);
-	    } else {
-                if (devices.indexOf(node.device) === -1) {
-                    node.warn(RED._("Device __device__ not found.", {device:node.device}));
-                    mraa.addSubplatform(mraa.GENERIC_FIRMATA, devices[0]);
-                }
-                else {
-                    node.log(RED._("Connecting to __device__", {device:node.device}));
-                    mraa.addSubplatform(mraa.GENERIC_FIRMATA, node.device);
-                }
-            }
-        });
-    }
-    RED.nodes.registerType("arduino-device",ArduinoDevice);
 
     function curieIMU(n){
         RED.nodes.createNode(this,n);
 
         // Properties
         this.name = n.name;
-	this.device = n.device;
 	this.mode = n.mode;
         this.interval = n.interval
 	this.imu = new curieImu.CurieImu();
@@ -71,10 +43,4 @@ module.exports = function(RED){
         });
     }
     RED.nodes.registerType('upm-curieimu', curieIMU);
-
-    RED.httpAdmin.get("/arduinodevices", RED.auth.needsPermission("arduino.read"), function(req,res) {
-        firmata.list(function (err, devices) {
-            res.json(devices);
-        });
-    });
 }
